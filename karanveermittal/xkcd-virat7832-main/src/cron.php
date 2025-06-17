4f825cecec9863020d84ad58c67e865aca8df728
Karanveer Mittal <karan.mittal2579@gmail.com>
2025-06-17 23:25:20 +0530

    Delete karanveermittal/xkcd-virat7832-main directory

--- karanveermittal/xkcd-virat7832-main/src/cron.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'functions.php';
-
-// Fetch HTML content of a random XKCD comic
-$comicHtml = fetchAndFormatXKCDData();
-
-// Send it to all subscribers
-sendXKCDUpdatesToSubscribers();
